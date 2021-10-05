bf26a814b6119f9526422fa8503b7db18b0b38ab
thiago <thiagofreitascarneiro@hotmail.com>
2021-10-05 11:34:44 -0300

    desafio com objetos

---
+++ Desafio_Objetos/CovertingsObjects.js
@@ -0,0 +1,19 @@
+//Write a function that converts an object into an array, 
+//where each element represents a key-value pair in the form of an array.
+
+const cabecalho = {
+    a: 1,
+    b: 2,
+    c: 3,
+}
+
+let lista = []
+
+
+
+
+function toArray(obj) {
+	return Object.entries(obj);
+}
+
+console.log(toArray(cabecalho))
