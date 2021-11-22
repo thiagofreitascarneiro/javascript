567529f04b974f11c6d453dfd2290982603e1035
unknown <thiagofreitascarneiro@hotmail.com>
2021-11-22 09:40:01 -0300

    axios request

--- ajax/server.js
+++ ajax/server.js
@@ -30,4 +30,11 @@ app.post('/upload', (req, res) => {
 
 })
 
+app.get('/parOuImpar', (req, res) => {
+   const par = parseInt(req.query.numero) % 2 === 0
+   res.send({
+       resultado: par ? 'par' : 'impar'
+   })
+})
+
 app.listen(8080, () => console.log('Executando...'))
