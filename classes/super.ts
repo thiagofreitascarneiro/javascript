55870968f7948b0b21c0edb4358448e83da5db7d
thiago <thiagofreitascarneiro@hotmail.com>
2022-07-05 14:18:06 -0300

    add heranca e super

--- classes/super.ts
+++ classes/super.ts
@@ -20,20 +20,32 @@ export class Pessoa {
 }
 export class Aluno extends Pessoa {
     getNomeCompleto(): string {
-        return 'isso vem do aluno' + this.nome + ' ' + this.sobrenome;
+        console.log('Fazendo algo antes');
+        return super.getNomeCompleto();
     }
 
 }
 export class Cliente extends Pessoa {
+    constructor(
+         nome: string, 
+         sobrenome: string, 
+         idade: number,
+         cpf: string,
+         public sala: string,
+    ) {
+        super(nome, sobrenome, idade, cpf);
+    }
+
     getNomeCompleto(): string {
-        return 'vem do cliente ' + this.nome + ' ' + this.sobrenome;
+        return 'vem do cliente ' + this.nome + ' ' + this.sobrenome + this.sala;
     }
 }
 
 const aluno = new Pessoa('Bob', 'Juniors', 30, '000.000.000.00');
 const pessoa = new Aluno('Luiz', 'Miranda', 20, '000.000.000.00');
-const cliente = new Cliente('Jo', 'Bill', 23, '000.000.000-00');
+const cliente = new Cliente('Jo', 'Bill', 23, '000.000.000-00', 'terceiro b');
 
 console.log(aluno);
 console.log(pessoa.getNomeCompleto());
 console.log(cliente.getNomeCompleto());
+console.log(cliente);
