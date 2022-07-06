ea89846c140011013bfd4c5ad9590447d9fa67e4
Thiago <thiagofreitascarneiro@hotmail.com>
2022-07-06 20:41:42 -0300

    add types

---
+++ classes/implementando-tipos.ts
@@ -0,0 +1,16 @@
+interface TipoPessoa  {
+     nome: string;
+     sobrenome: string;
+     nomeCompleto(): string;
+};
+
+export class Pessoa implements TipoPessoa {
+    constructor(public nome: string, public sobrenome: string) {}
+
+    nomeCompleto(): string {
+        return this.nome + ' ' + this.sobrenome;
+    }
+}
+
+const pessoa = new Pessoa('Luiz', 'Miranda');
+console.log(pessoa.nomeCompleto());
